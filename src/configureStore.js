2454c38dff5e4f011a007910afb0d9bfabb3db68
antonv <toxa07@gmail.com>
2017-01-17 16:41:21 +0100

    remote redux dev tools are working, though quite slow

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,17 +1,20 @@
 import { createStore, applyMiddleware } from 'redux';
 import createLogger from 'redux-logger';
 import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'remote-redux-devtools';
 import reducer from './reducers';
 
 const configureStore = () => {
   const middleware = [thunk];
+  const composeEnhancers = composeWithDevTools({ realtime: true, port: 8000 });
+  console.log('!!!!!!!!! NODE_ENV', process.env.NODE_ENV);
   if (process.env.NODE_ENV !== 'production') {
     middleware.push(createLogger());
   }
 
   return createStore(
     reducer,
-    applyMiddleware(...middleware)
+    composeEnhancers(applyMiddleware(...middleware))
   );
 };
 
