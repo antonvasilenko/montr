74fb67011466689e043c9486d02fdec94ec24846
antonv <toxa07@gmail.com>
2017-01-18 22:56:55 +0100

    use react-native-debugger for debugging insted of chrome default debugger.  (in that way we have console, network log, and redux store UI in one place)

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,16 +1,21 @@
 import { createStore, applyMiddleware } from 'redux';
 import createLogger from 'redux-logger';
 import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'remote-redux-devtools';
-import reducer from './reducers';
+// import { composeWithDevTools } from 'remote-redux-devtools';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import reducer from './modules';
 
 const configureStore = () => {
   const middleware = [thunk];
-  const composeEnhancers = composeWithDevTools({ realtime: true, port: 8000 });
+  const composeEnhancers = composeWithDevTools({ realtime: true, port: 8097 });
   console.log('!!!!!!!!! NODE_ENV', process.env.NODE_ENV);
   if (process.env.NODE_ENV !== 'production') {
     middleware.push(createLogger());
   }
+  // https://github.com/jhen0409/react-native-debugger#inpsect-network-requests-by-network-tab-of-chrome-devtools-see-also-15
+  const xhr = global.originalXMLHttpRequest ?
+    global.originalXMLHttpRequest : global.XMLHttpRequest;
+  global.XMLHttpRequest = xhr;
 
   return createStore(
     reducer,
